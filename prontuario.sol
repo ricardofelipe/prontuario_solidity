b9ad2b7205b79959701b917468b238c9932da1fb
Ricardo <ricardofelipe@outlook.com>
2019-03-13 13:47:46 -0300

    Adicionando Acessos

--- prontuario.sol
+++ prontuario.sol
@@ -4,21 +4,21 @@ pragma experimental ABIEncoderV2;
 
 contract Prontuario {
     
-    address public system;
+    address public creator;
     
     constructor () public {
-        system = msg.sender;
+        creator = msg.sender;
     }
     
     struct Exame {
         string nome;
         string resultado;
     }
+    
     struct Paciente {
-        address addr;
+        address paciente;
         string nome;
         int idade;
-        string genero;
         Exame[] exames;
         address[] acessos;
         mapping(address => bool) roles;
@@ -26,69 +26,88 @@ contract Prontuario {
 
     mapping(address => Paciente) private pacientesLista;
 
-    function getQtd(address _address )  public view returns (uint) {
+    function getQtdExame(address _paciente ) public view returns (uint) {
         uint _qtdexame;
         
-        if(msg.sender == system){
-            _qtdexame = pacientesLista[_address].exames.length;   
-        }else{
+        // Verifica se o usuario possui acesso ao paciente solicitado.
+        if(isAssignedRole(_paciente, msg.sender)){
+            _qtdexame = pacientesLista[_paciente].exames.length;   
+        }else{//caso nao possua acesso é retornado somente a quantidade de exames do prorpio usuario
             _qtdexame = pacientesLista[msg.sender].exames.length;   
         }
         return _qtdexame;
     }
     
-    function addPaciente(address _address, string memory _nome) public {
-        
-       
-        if(msg.sender == system){
-            pacientesLista[_address].addr = _address; 
-            pacientesLista[_address].nome = _nome;
-        }
+    //Somente o criador pode adicionar novos pacientes.
+    function addPaciente(address _paciente, string memory _nome, int _idade) hasCreator() public {
+        pacientesLista[_paciente].paciente = _paciente; 
+        pacientesLista[_paciente].nome = _nome;
+        pacientesLista[_paciente].idade = _idade;
     }
 
-    function addExames(address _address, string memory _nome, string memory _resultado) public {
+    function addExames(address _paciente, string memory _nome, string memory _resultado) public {
         Exame memory _exame;
         
-        _exame.nome = _nome;
-        _exame.resultado = _resultado;
-         
-        if (msg.sender == system) {
-            pacientesLista[_address].exames.push(_exame);
+        //Verifica se o usuario possui acesso ao paciente solicitado.
+        if(isAssignedRole(_paciente, msg.sender)){
+            _exame.nome = _nome;
+            _exame.resultado = _resultado;
+            pacientesLista[_paciente].exames.push(_exame);
         }
     }
     
-    function getExames(address _address) public view returns (Exame[] memory) {
+    function getExames(address _paciente) public view returns (Exame[] memory) {
         Exame[] memory _exames;
         
-        if(msg.sender == system){
-            _exames = pacientesLista[_address].exames;  
-        }else{
+        //Verifica se o usuario ossui acesso ao paciente solicitado.
+        if(isAssignedRole(_paciente, msg.sender)){
+            _exames = pacientesLista[_paciente].exames;  
+        }else{//caso nao possua acesso é retornado somente os exames do prorpio usuario
             _exames = pacientesLista[msg.sender].exames;   
         }
         
         return _exames;
     }
     
-    function addAcesso(address _address, address _acesso) public {
-        if (msg.sender == system) {
-            pacientesLista[_address].acessos.push(_acesso);
+    function getPaciente(address _paciente) public view returns (string memory _nome, int _idade) {
+        Paciente memory __paciente;
+        
+        //Verifica se o usuario ossui acesso ao paciente solicitado.
+        if(isAssignedRole(_paciente, msg.sender)){
+            __paciente = pacientesLista[_paciente];  
+        }else{//caso nao possua acesso é retornado somente os exames do prorpio usuario
+            __paciente = pacientesLista[msg.sender];   
         }
+        
+        return (__paciente.nome, __paciente.idade);
     }
     
-    function assignRole (address paciente, address entity) hasRole(paciente) public{
-        pacientesLista[paciente].roles[entity] = true;
+    //Somente o criador pode adicionar acesso ao paciente solicitado.
+    function assignRole (address _paciente, address entity) hasCreator() public{
+        pacientesLista[_paciente].roles[entity] = true;
     }
 
-    function unassignRole (address paciente, address entity) hasRole(paciente) public{
-        pacientesLista[paciente].roles[entity] = false;
+    //Somente o criador pode remover acesso ao paciente solicitado.
+    function unassignRole (address _paciente, address entity) hasCreator() public{
+        pacientesLista[_paciente].roles[entity] = false;
     }
-
-    function isAssignedRole (address paciente, address entity) public view returns (bool){
-        return pacientesLista[paciente].roles[entity];
+    
+    //Verificação se o usuario possui acesso ao paciente solicitado.
+    function isAssignedRole (address _paciente, address entity) public view returns (bool){
+        return pacientesLista[_paciente].roles[entity];
     }
-  
-    modifier hasRole (address paciente) {
-        if (!pacientesLista[paciente].roles[msg.sender] && msg.sender != system) {
+    
+    //Função para verificar se possui acesso.
+    modifier hasRole (address _paciente) {
+        if (!pacientesLista[_paciente].roles[msg.sender] && msg.sender != creator) {
+            revert();
+        }
+        _;
+    }
+    
+    //Função para verificar se é o criador.
+    modifier hasCreator () {
+        if (msg.sender != creator) {
             revert();
         }
         _;
